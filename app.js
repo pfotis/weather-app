a954e298708035748f5f5f04f1eb9f8bca2c707d
peter fotis <pfotis@gmail.com>
2021-07-14 20:56:27 +1000

    add code

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ console.log("Starting")
 
 setTimeout(() => {
     console.log("2 Second Timer")
-},2000)
+}, 2000)
+
+setTimeout(() => {
+    console.log("0 Seconds Timer")
+}, 0)
 
 console.log("Stopping")
