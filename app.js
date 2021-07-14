e7c4fbb8fa31e1a26e1dbdbecc25e0b7e6611c66
peter fotis <pfotis@gmail.com>
2021-07-14 22:15:13 +1000

    create request print the weather

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=89906dfb7f555d9f1870b074e1ec1aba&query=37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    console.log(response)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current)
+    console.log(`It is currently ${response.body.current.temperature} degress out. It feels like ${response.body.current.feelslike} degress out.`)
 })
+
