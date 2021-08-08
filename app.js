1fbf0a96ab6c27c9717a6e6d40860fc4ed22a2e2
peter fotis <pfotis@gmail.com>
2021-08-08 23:16:12 +1000

    create function in app.js

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-
-// const url = 'http://api.weatherstack.com/current?access_key=89906dfb7f555d9f1870b074e1ec1aba&query=&units=f'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if(error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degress out. It feels like ${response.body.current.feelslike} degress out.`)
-//     }
-// })
-
 
 geocode('Boston', (error, data) => {
     console.log('Error', error)
     console.log('Data' , data)
-})
-
-forecast(42.3605, -71.0596, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+    forecast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error', error)
+        console.log('Data', data)
+    })
 })
