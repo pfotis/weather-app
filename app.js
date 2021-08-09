641487b71fe6ffc2c1a089fe854315c9e95ffacb
peter fotis <pfotis@gmail.com>
2021-08-09 18:30:05 +1000

    complete the app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    if(error) {
-        return console.log(error)
-    } else {
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
-            if(error) {
-                return console.log(error)
-            } else {
-                console.log(data.location)
-                console.log(forecastData)
-            }
-        })
-    }   
-})
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    console.log(process.argv)
+    geocode(address, (error, data) => {
+        if(error) {
+            return console.log(error)
+        } else {
+            forecast(data.latitude, data.longitude, (error, forecastData) => {
+                if(error) {
+                    return console.log(error)
+                } else {
+                    console.log(data.location)
+                    console.log(forecastData)
+                }
+            })
+        }   
+    })
+}
